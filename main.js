c9406ff3b23e1b2dda6485037669ac898cb520cb
Aye Anca <ayelen.anca@gmail.com>
2023-08-28 15:29:10 -0300

    agregue mas json y organice

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ let comer;
 let noche = false;
 let sonido;
 let preguntas = []; //JSON
+let comidas = []; //JSON
+let formasDeAmor = []; //JSON
+let deNoche = []; //JSON
+
 //Constantes
 // Objeto Mascota
 const miMascota = {
@@ -10,6 +14,7 @@ const miMascota = {
     energia: 80,
     salud: 80,
     edad: 1,
+    dinero: 1,
 };
 //Constante para asegurarme que la mascota sigue viva
 const mascotaViva = () => miMascota.salud > 0 && miMascota.energia > 0;
@@ -24,179 +29,11 @@ const fondosRosas = document.querySelectorAll(".boton, .card");
 
 //ARRAYS QUE NO PASE A JSON PORQ EL EFECTO COMO UTILIZA "+" NO ME PERMITE IMPLEMENTARLO 
     // Array de comidas
-const comidas = [
-    {
-        nombre: "zanahoria",
-        efecto: {
-            salud: 10,
-            energia: 30,
-        },
-        mensaje:
-            "Le diste zanahoria. ¡Buena elección! La energía aumenta +30 y, como la zanahoria es muy sana, su salud aumenta +10.",
-    },
-    {
-        nombre: "hamburguesa",
-        efecto: {
-            salud: -30,
-            energia: 80,
-        },
-        mensaje: `Le diste una hamburguesa. Eso da mucha energía pero no es muy sano... le sube el colesterol a  tu mascota. La energía aumenta +80 pero la salud baja -30.`,
-    },
-    {
-        nombre: "sandia con vino",
-        efecto: {
-            salud: -100,
-            energia: -100,
-        },
-        mensaje:
-            "Le diste sandía con vino. ¡¿¿¿Qué estás mal de la cabeza???! ¡SOS UN ASESINO! ¡ESTOY LLAMANDO A LA POLICÍA!",
-    },
-    {
-        nombre: "pepino",
-        efecto: {
-            salud: 0,
-            energia: 20,
-        },
-        mensaje:
-            "Le diste pepino. Buena elección, es una comida muy sana pero no llena mucho. La energía aumenta +20",
-    },
-    {
-        nombre: "helado",
-        efecto: {
-            salud: -30,
-            energia: 40,
-        },
-        mensaje:
-            "Le diste helado. ¡Qué rico! Pero las mascotas no pueden comer eso, por lo que le da diabetes de mascotas. La energía aumenta +40 pero la salud se reduce -30",
-    },
-];
 
     // Array de formas de dar amor
-const formasDeAmor = [
-    {
-        nombre: "abrazo",
-        efecto: {
-            salud: 20,
-            energia: 5,
-        },
-        mensaje: `Le diste un abrazo a tu mascota. Tu mascota es muy feliz y no sólo aumenta su salud +20, sino también, su energía +5.`,
-    },
-    {
-        nombre: "leerle un cuento",
-        efecto: {
-            salud: -20,
-            energia: -10,
-        },
-        mensaje: `Le leíste un cuento a tu mascota. Tu mascota no te escuchó, tu mascota se puso a jugar y se golpeó la cabeza corriendo. Baja su salud -20 y su energía -10.`,
-    },
-    {
-        nombre: "palmada en la espalda",
-        efecto: {
-            salud: -20,
-            energia: -5,
-        },
-        mensaje: `Le diste una palmada en la espalda a tu mascota. Esa frialdad le destruye el corazón a tu mascota. Baja su salud -20 y su energía -5.`,
-    },
-    {
-        nombre: "beso",
-        efecto: {
-            salud: 40,
-            energia: 0,
-        },
-        mensaje: `Le diste un beso a tu mascota. Tu mascota es muy feliz. Su salud aumenta +40`,
-    },
-    {
-        nombre: "jugar",
-        efecto: {
-            salud: 80,
-            energia: -20,
-        },
-        mensaje: `Jugaste con tu mascota. Tu mascota es muy feliz pero gastó mucha energía. Su salud aumenta +80 y su enegía baja -20`,
-    },
-];
+
     //Array de posibilidades para cada día
-const deNoche = [
-    {
-        nombre: "sueño",
-        mensaje: `Durante la noche tu mascota soñó que el perro del vecino le pinchaba su pelota favorita. Se puso muy triste y se levantó asustado. Perdió -25 de salud y de energía.`,
-        efecto: {
-            salud: -25,
-            energia: -25,
-        },
-    },
-    {
-        nombre: "techo",
-        mensaje: `Durante la noche, mientras tu mascota dormía, se inundó el departamento del vecino de arriba y el techo de tu casa se cayó sobre tu mascota, que se asustó y se lastimó un poco. Perdió -40 de salud y de energía`,
-        efecto: {
-            salud: -40,
-            energia: -40,
-        },
-    },
-    {
-        nombre: "ovnis",
-        mensaje: `Durante la noche vinieron ovnis que abdujeron a tu mascota. Le hicieron estudios completos y, cuando se puso a morder los botones de la nave, trajeron a tu mascota devuelta a casa. Perdió -30 de salud y de energía`,
-        efecto: {
-            salud: -30,
-            energia: -30,
-        },
-    },
-    {
-        nombre: "fiebre",
-        mensaje: `Durante la noche a tu mascota le subió la fiebre. Por suerte ya está mejor pero gastó mucha energía y salud. Perdió -50 de salud y de energía`,
-        efecto: {
-            salud: -50,
-            energia: -50,
-        },
-    },
-    {
-        nombre: "insomnio",
-        mensaje: `Durante la noche tu mascota no pudo dormir por tener insomnio. Su energía disminuyó -30.`,
-        efecto: {
-            salud: 0,
-            energia: -30,
-        },
-    },
-    {
-        nombre: "bien",
-        mensaje: `Durante la noche tu mascota durnmió plácidamente. Recuperando +40 de salud y de energía.`,
-        efecto: {
-            salud: +40,
-            energia: +40,
-        },
-    },
-    {
-        nombre: "fiesta",
-        mensaje: `Durante la noche tu mascota no pudo dormir casi nada porque los vecinos hicieron una fiesta hasta la madrugrada. Perdió -35 de energía.`,
-        efecto: {
-            salud: 0,
-            energia: -35,
-        },
-    },
-    {
-        nombre: "sillón",
-        mensaje: `Durante la noche tu mascota mordió el sillón y lo rompió. Te enojaste y le ordenaste ir a dormir afuera. Tu mascota se puso triste. Perdió -30 de salud y de energía`,
-        efecto: {
-            salud: -30,
-            energia: -30,
-        },
-    },
-    {
-        nombre: "sueño lindo",
-        mensaje: `Durante la noche tu mascota tuvo un hermoso sueño donde comía croquetas. ¡Recuperó +30 de salud y de energía!.`,
-        efecto: {
-            salud: +30,
-            energia: +30,
-        },
-    },
-    {
-        nombre: "ratón",
-        mensaje: `Durante la noche tu mascota comenzó a perseguir un ratón. En su desenfreno no vió los muebles y se llevó puesta una silla. Perdió -40 de salud y de energía`,
-        efecto: {
-            salud: -40,
-            energia: -40,
-        },
-    },
-];
+
 
 //inicio del Juego, recarga partida anterior o empieza una nueva
 function cargarPartida() {
@@ -342,9 +179,11 @@ function actualizarInfoEnPantalla() {
     const infoSalud = document.getElementById("infoSalud");
     const infoEnergia = document.getElementById("infoEnergia");
     const infoEdad = document.getElementById("infoEdad");
+    const infoDinero = document.getElementById("infoDinero")
     infoSalud.textContent = "Salud = " + miMascota.salud;
     infoEnergia.textContent = "Energía = " + miMascota.energia;
     infoEdad.textContent = "Días de vida = " + miMascota.edad;
+    infoDinero.textContent = "$" + miMascota.dinero;
     guardarProgreso();
 }
 function maxMin() {
@@ -353,12 +192,32 @@ function maxMin() {
     miMascota.salud = Math.max(miMascota.salud, 0);
     miMascota.energia = Math.max(miMascota.energia, 0);
 }
+function incrementarDinero() {
+    const cantidadGanada = 3; 
+    miMascota.dinero += cantidadGanada;
+
+}
+/****************************************************************************/
+/*                             COMPRAR ESTILOS                              */
+/****************************************************************************/
+// function comprar () {
+//     const mascotasDiv = document.getElementById("mascotas");
+//     const seleccionMascotaDiv = document.getElementById("seleccionMascota");
+//     mascotasDiv.style.display = "none";
+//     seleccionMascotaDiv.style.display = "block";
+// }
+// function volverAtras() {
+//     const mascotasDiv = document.getElementById("mascotas");
+//     const seleccionMascotaDiv = document.getElementById("seleccionMascota");
+//     mascotasDiv.style.display = "block";
+//     seleccionMascotaDiv.style.display = "none";
+// }
 
 // FUNCIONES ASINCRONICAS 
 //cargar preguntas 
 async function cargarPreguntasDesdeJSON() {
     try {
-        const response = await fetch("./preguntas.json");
+        const response = await fetch("./json/preguntas.json");
         const data = await response.json();
         return data;
     } catch (error) {
@@ -370,6 +229,48 @@ cargarPreguntasDesdeJSON().then((data) => {
     preguntas = data;
 });
 
+//cargar comida 
+async function cargarComidasDesdeJSON() {
+    try {
+        const response = await fetch("./json/comidas.json");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al cargar las comidas:", error);
+        return [];
+    }
+}
+cargarComidasDesdeJSON().then((data) => {
+    comidas = data;
+});
+//cargar formasDeAmor
+async function cargarFormasDeAmorDesdeJSON() {
+    try {
+        const response = await fetch("./json/formasdeamor.json");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al cargar las formas de dar amor:", error);
+        return [];
+    }
+}
+cargarFormasDeAmorDesdeJSON().then((data) => {
+    formasDeAmor = data;
+});
+//cargar deNoche 
+async function cargarDeNocheDesdeJSON() {
+    try {
+        const response = await fetch("./json/denoche.json");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al cargar de noche:", error);
+        return [];
+    }
+}
+cargarFormasDeAmorDesdeJSON().then((data) => {
+    deNoche = data;
+});
 //DAR COMIDA A LA MASCOTA
 function darComida(opcionComida) {
     const azar = Math.floor(Math.random() * preguntas.length);
@@ -413,6 +314,7 @@ function darComida(opcionComida) {
                     } else {
                         sonido = new Audio("./audios/dia.mp3");
                         sonido.play();
+                        incrementarDinero();
                         actualizarInfoEnPantalla();
                         cambiarTextoCard(comidaElegida.mensaje);
                         noche = false;
@@ -472,6 +374,7 @@ function darAmor(opcionAmor) {
                     } else {
                         sonido = new Audio("./audios/dia.mp3");
                         sonido.play();
+                        incrementarDinero();
                         actualizarInfoEnPantalla();
                         cambiarTextoCard(amorElegido.mensaje);
                         noche = false;
